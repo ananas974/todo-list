e42c0aa3957c67428987daf130c7b861052d8b3a
Sabrina MAMODRAZA <sabrinamamodraza@MacBook-Air-de-Sabrina.local>
2024-03-17 22:55:15 +0400

    ceci est mon programme

---
+++ src/script.js
@@ -0,0 +1,53 @@
+let list = [];
+
+const createItem = (itemText) => {
+    // creer un element li
+    const li = document.createElement('li');
+
+    // ajouter le texte de l'item
+    li.innerHTML = itemText;
+
+    // retourner l'element li
+    return li;
+
+    // ce qui donnera
+    // <li>{itemText}</li>;
+}
+
+const renderList = (list) => {
+    // selectionner l'element qui contiendra la liste
+    const listContainer = document.getElementById('list');
+
+    // on vide la liste pour re-afficher les items de zero, sinon on aurait des doublons
+    listContainer.innerHTML = '';
+
+    // pour chaque item dans la liste: c good 
+   
+        // creer un element li
+
+        // l'ajouter comme un enfant de listContainer (qui sera donc le parent)
+        for (const itemText of list) {
+            const item = createItem(itemText)
+         console.log (itemText)
+         listContainer.appendChild(item)
+        }        
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    input.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter') {
+            // on recupere le texte de l'input
+            const itemText = input.value;
+
+            // On ajoute le texte a la liste de todo
+            
+list.push(itemText)
+console.log(list)
+            // On affiche la liste dans le DOM
+            renderList(list);
+        }
+    });
+});
